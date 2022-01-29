bd4e3248d573b3dad2d24845428edfb0d05b44e4
Johannes Reichle <jreichle@hm.edu>
2022-01-29 16:06:29 +0100

    Finish Conv Chapter, add Figures

--- figure-code/Matlab/activationFunc.m
+++ figure-code/Matlab/activationFunc.m
@@ -1,18 +1,16 @@
 function [x] = activationFunc()
 
-
-
 h1 = fplot(@(x) max(0,x), [-2.5 2.5]);
 hold on
 h2 = fplot(@(y) (1 / (1 + exp(-y))), [-2.5 2.5]);
-hold on
-h3 = fplot(@(z) log(1+exp(z)), [-2.5 2.5]);
-set([h1 h2 h3],'LineWidth',2)
+set([h1 h2],'LineWidth',2)
 
 grid on
+
+ylim([-0.1 1.5])
 xlabel('x');
 ylabel('z');
-lg = legend({'ReLU','sigmoid','sofmax'},'Location','northwest');
+lg = legend({'ReLU','sigmoid'},'Location','northwest');
 lg.FontSize = 12;
 set(gca,'Fontsize',12);
 
